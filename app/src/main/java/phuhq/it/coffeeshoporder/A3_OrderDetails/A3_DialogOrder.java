255b3577860123d6c1b5fc065c3b365269de5756
QuocPhuDEV <phuhq.it@gmail.com>
2020-04-08 11:02:08 +0700

    08/03/2020

---
+++ app/src/main/java/phuhq/it/coffeeshoporder/A3_OrderDetails/A3_DialogOrder.java
@@ -0,0 +1,16 @@
+package phuhq.it.coffeeshoporder.A3_OrderDetails;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import phuhq.it.coffeeshoporder.R;
+
+public class A3_DialogOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a3_dialog_order);
+    }
+}
---
+++ app/src/main/java/phuhq/it/coffeeshoporder/A3_OrderDetails/A3_OrderDetails.java
@@ -0,0 +1,16 @@
+package phuhq.it.coffeeshoporder.A3_OrderDetails;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import phuhq.it.coffeeshoporder.R;
+
+public class A3_OrderDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a3_order_details);
+    }
+}

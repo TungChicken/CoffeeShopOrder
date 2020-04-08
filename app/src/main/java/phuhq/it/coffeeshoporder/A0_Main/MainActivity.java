ee98448c55f5fef495a78da0e3a27748f27c268b
QuocPhuDEV <phuhq.it@gmail.com>
2020-04-08 20:08:11 +0700

    08/03/2020

--- app/src/main/java/phuhq/it/coffeeshoporder/A0_Main/MainActivity.java
+++ app/src/main/java/phuhq/it/coffeeshoporder/A0_Main/MainActivity.java
@@ -1,32 +1,39 @@
 package phuhq.it.coffeeshoporder.A0_Main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.ArrayList;
 
+import phuhq.it.coffeeshoporder.A3_OrderDetails.A3_DialogOrder;
 import phuhq.it.coffeeshoporder.R;
 
 import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
     //region AVAILABLE
-
+    private CardView btnCoffee;
     //endregion
+
+    //region FROM EVENT
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mainLoad();
     }
+    //endregion
 
     //region ADD CONTROLS AND LOAD
     public void addControls() {
-
+        btnCoffee = findViewById(R.id.view3);
     }
 
     public void mainLoad() {
@@ -34,6 +41,12 @@ public class MainActivity extends AppCompatActivity {
     }
     //endregion
 
-    //region PRODUCT RECYCLER VIEW
+    //region ON TOUCH
+    public void onCoffeeClick(View view) {
+//        Intent order = new Intent(MainActivity.this, A3_DialogOrder.class);
+//        startActivity(order);
+        A3_DialogOrder dialogOrder = new A3_DialogOrder(MainActivity.this);
+        dialogOrder.show();
+    }
     //endregion
 }
--- app/src/main/java/phuhq/it/coffeeshoporder/A3_OrderDetails/A3_DialogOrder.java
+++ app/src/main/java/phuhq/it/coffeeshoporder/A3_OrderDetails/A3_DialogOrder.java
@@ -1,16 +1,98 @@
 package phuhq.it.coffeeshoporder.A3_OrderDetails;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import phuhq.it.coffeeshoporder.R;
 
-public class A3_DialogOrder extends AppCompatActivity {
+public class A3_DialogOrder extends Dialog implements View.OnClickListener {
+    //region AVAILABLE
+    private SeekBar seekBar;
+    private TextView tvOrderCount;
 
+    public Activity activity;
+    public Dialog dialog;
+    //endregion
+
+    //region FORM EVENTS
+    public A3_DialogOrder(@NonNull Context context) {
+        super(context);
+    }
+
+    public A3_DialogOrder(@NonNull Context context, int themeResId) {
+        super(context, themeResId);
+    }
+
+    protected A3_DialogOrder(@NonNull Context context, boolean cancelable, @Nullable OnCancelListener cancelListener) {
+        super(context, cancelable, cancelListener);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.a3_dialog_order);
+        mainLoad();
+        setDataSeekBar();
     }
+
+    private void addControls() {
+        seekBar = findViewById(R.id.seekBar);
+        tvOrderCount = findViewById(R.id.textView26);
+    }
+
+    private void mainLoad() {
+        addControls();
+    }
+
+    //endregion
+
+    //region SEEKBAR
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setDataSeekBar() {
+        try {
+            this.seekBar.setMax(10);
+            this.seekBar.setMin(1);
+            tvOrderCount.setText(String.valueOf(seekBar.getProgress()));
+
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                int progress = 0;
+
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+                    progress = progressValue;
+                    tvOrderCount.setText(String.valueOf(progress));
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    tvOrderCount.setText(String.valueOf(progress));
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+    //endregion
 }

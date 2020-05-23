d9a97cbdf175ad8791c2987d6b80ae80ee82b52c
QuocPhuDEV <phuhq.it@gmail.com>
2020-05-23 09:25:48 +0700

    11/04/2020

---
+++ app/src/main/java/phuhq/it/coffeeshoporder/G_Setting/BootReceiver.java
@@ -0,0 +1,18 @@
+package phuhq.it.coffeeshoporder.G_Setting;
+
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import phuhq.it.coffeeshoporder.A1_Intro.A1_Intro;
+
+public class BootReceiver extends BroadcastReceiver {
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent myIntent = new Intent(context, A1_Intro.class);
+        myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(myIntent);
+    }
+}
